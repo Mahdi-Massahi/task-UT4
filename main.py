f83e0b2079bb414439c433397b61bf3d8cc5485b
Mahdi-Massahi <Mahdi.Massahi@gmail.com>
2023-08-22 14:28:21 +0330

    Add logging to the code

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from datetime import date
+import logging
 
 app = FastAPI()
 
+logging.basicConfig(level=logging.INFO)
+
 @app.post("/")
 def get_current_date():
+    logging.info('Received POST request')
     return {"current_date": str(date.today())}
